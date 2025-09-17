4771ed9fd3d9f1c065155ed19141e40c5d993860
emiliotogashi <emilio.togashi@gmail.com>
2025-09-16 23:43:45 -0300

    Create ignupPage.js

---
+++ cypress/pages/ignupPage.js
@@ -0,0 +1,19 @@
+import BasePage from './BasePage';
+
+class SignupPage extends BasePage {
+  fillSignupName(name) {
+    cy.get('input[name="name"]').type(name);
+  }
+  
+  fillSignupEmail(email) {
+    cy.get('input[data-qa="signup-email"]').type(email);
+  }
+
+  submitSignup() {
+    cy.get('button[data-qa="signup-button"]').click();
+  }
+
+  // métodos para preencher formulário de conta, etc.
+}
+
+export default SignupPage;
