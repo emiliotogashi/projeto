ace0495b4b36c8c22e77279282b1dadfe53d0d78
emiliotogashi <emilio.togashi@gmail.com>
2025-09-16 23:39:51 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,18 @@
-Cypress.Commands.add('search',term=>{
- cy.get('input[type="text"]').should('be.visible')
- .clear()
- .type(`${term}{enter}`)
-})
+// comandos customizados
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+
+Cypress.Commands.add('login', (email, password) => {
+  const home = new HomePage();
+  const login = new LoginPage();
+  home.visit();
+  home.clickSignupLogin();
+  login.fillLoginEmail(email);
+  login.fillLoginPassword(password);
+  login.submitLogin();
+});
+
+Cypress.Commands.add('logout', () => {
+  // assume after login, existe um botão "Logout"
+  cy.get('a[href="/logout"]').click();
+});
