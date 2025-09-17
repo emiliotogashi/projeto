850af1050485b5377862c1e31a5f0661fbe9cdd5
emiliotogashi <emilio.togashi@gmail.com>
2025-09-16 23:37:47 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'http://localhost:3000'
-  },
-  
-  video: false,
-})
+    baseUrl: 'https://www.automationexercise.com',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/index.js',
+    defaultCommandTimeout: 10000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true
+  }
+});
